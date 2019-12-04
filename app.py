b34293dd23b645f8403e0aa2f70cc6b1ca98d47d
elammend <eddy.lam97@gmail.com>
2019-12-03 22:15:51 -0800

    first commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from PIL import Image
+from io import BytesIO
+import base64,re,time
+import numpy as np
+import imutils
+import cv2
+import os
+app = Flask(__name__)
+CORS(app)
+
+def convert_and_save(b64_string):
+    base64_data = re.sub('^data:image/.+;base64,', '', b64_string)
+    byte_data = base64.b64decode(base64_data)
+    image_data = BytesIO(byte_data)
+    img = Image.open(image_data)
+    t = time.time()
+    print(os.getcwd() + str(t))
+    img.save('fromclient.jpg')
+
+@app.route('/')
+def hello_world():
+    print(cv2)
+    return 'Hello World!'
+
+@app.route('/getSkeleton', methods = ['POST'])
+def get_skeleton():
+    base_64_code = request.json["image"]
+    print(base_64_code)
+    convert_and_save((base_64_code) )
+    os.system("python final_model_script.py")
+    with open("output.jpg", "rb") as img_file:
+        my_string = base64.b64encode(img_file.read())
+    print(my_string.decode('utf-8'))
+    f = open('predicted_pose.txt')
+    line = f.readline()
+    f.close()
+    return jsonify(name=line, code=my_string.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ final_model_script.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import sklearn
+import os
+from random import randint
+from os.path import isfile, join
+from keras.models import load_model
+import A2_Functions as a2
+
+
+# In[13]:
+
+
+model = load_model("yoga_pals_model.h5")
+save_file = "output.jpg"
+read_file = "fromclient.jpg"
+pose_file = "predicted_pose.txt"
+
+
+# In[14]:
+
+
+detected_keypoints, frameClone, image = a2.perform_keypoint_analysis(read_file)
+missing = []
+found = []
+i = 0
+for el in detected_keypoints:
+    if el == []:
+        missing.append(i)
+    else:
+        found.append(i)
+    i += 1
+if a2.apply_filter(missing) is True:
+    results = a2.scale_keypoints(read_file)
+    predict = model.predict(np.array([results]))
+    predicted_pose, confidence = a2.predict_pose(predict)
+    with open(pose_file, "w") as fp:
+        fp.write("With " + str(confidence) + "% confidence, the predicted pose is " + predicted_pose)
+    print("Our model predicted that your pose is: " + predicted_pose + (" with %.2f" % (confidence * 100)) + "% confidence.")
+else:
+    with open(pose_file, "w") as fp:
+        fp.write("Error: Photo did not pass filter.")
+    print("Error: Photo did not pass filter.")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
